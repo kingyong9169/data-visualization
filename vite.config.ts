27a330e5e131f2bc9d1c6b22366919669657f339
동동이 <drdragon777@naver.com>
2023-04-24 16:09:15 +0900

    setting: project initial setting(react, typescript, sass, vite)

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import '@styles/variables.scss';
+          @import '@styles/colors.scss';
+        `,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
