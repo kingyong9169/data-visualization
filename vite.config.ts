5fe6ca8a70c4c78e8e0b3055ebf14aa4b021e503
동동이 <drdragon777@naver.com>
2023-04-25 22:18:35 +0900

    feat: add responsive-chart mixin

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
         additionalData: `
           @import '@styles/variables.scss';
           @import '@styles/colors.scss';
+          @import '@styles/mixin.scss';
         `,
       },
     },
