1dd6b1fa352e5793bf41bbe828ba505935f0a5bb
동동이 <drdragon777@naver.com>
2023-05-02 01:49:11 +0900

    chore: remove ProjectStatistics in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import $ from '@styles/app.module.scss';
 
 import AgentThread from './components/AgentThread';
-import ProjectStatistics from './components/ProjectStatistics';
 import SpotInfo from './components/SpotInfo';
 import usePollingController from './hooks/usePollingController';
 import SpotMethodInfo from './components/SpotMethodInfo';
@@ -15,7 +14,6 @@ function App() {
       <SpotInfo />
       <SpotMethodInfo />
       <AgentThread />
-      <ProjectStatistics />
       <SpotTransaction />
     </main>
   );
