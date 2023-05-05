9f0426ff104ee37109fbb7d3767d0501a864f661
동동이 <drdragon777@naver.com>
2023-05-05 20:48:52 +0900

    chore: add title style

--- src/components/shared/SubTitle/index.tsx
+++ src/components/shared/SubTitle/index.tsx
@@ -1,7 +1,9 @@
+import $ from './style.module.scss';
+
 type Props = {
   text: string;
 };
 
 export default function SubTitle({ text }: Props) {
-  return <h2>{text}</h2>;
+  return <h2 className={$['sub-title']}>{text}</h2>;
 }
